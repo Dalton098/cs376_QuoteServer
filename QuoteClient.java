d5268b41959cdfee2f596bbac7bc5e1c3b9983a3
Dalton <dalton098@ymail.com>
2020-03-10 14:52:06 -0400

    Finished except analysis. Client and Server done as well as extra credit

---
+++ QuoteClient.java
@@ -0,0 +1,88 @@
+import java.net.*;
+import java.io.*;
+
+/**
+ * Quote Client
+ * Takes in command line argument of the form [numQuotes] [genre] when called
+ * and interfaces with the QuoteServer to retrieve quotes from the files included
+ * in the submission. Then prints out the retrieved quotes
+ * 
+ * Instructions:
+ * -   To compile the file run javac QuoteClient.java 
+ *      from a terminal while in the directory containing the file
+ * -   NOTE: The QuoteServer should be running before attempting to run the client
+ * -   To run the file (after it has been compiled): java QuoteClient [numQuotes] [genre]
+ * 
+ * CS 376
+ * @author Dalton Rothenberger
+ */
+public class QuoteClient {
+
+    public static void main(String[] args) {
+        int numInputs = 2;
+        if (args.length == numInputs) {
+
+            int numQuotes = Integer.parseInt(args[0]);
+            String genre = args[1].toLowerCase();
+
+            try {
+
+                // make connection to server socket
+                Socket sock = new Socket("127.0.0.1", 6013);
+
+                // Output to server
+                PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
+
+                // Input from server
+                BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+
+                // Sending number of quotes and genre to server
+                sendMessage(out, Integer.toString(numQuotes));
+                sendMessage(out, genre);
+
+                // Printing the quotes from the server
+                printQuotes(in, numQuotes);
+
+                // Close the socket connection
+                sock.close();
+            } catch (IOException ioe) {
+                System.out.println("Error in client: An exception has occurred.");
+            }
+
+        } else {
+            System.out.println(
+                    "Error in client: Expected inputs format is as follows: java QuoteClient [numQuotes] [genre]");
+        }
+
+    }
+
+    /**
+     * Will communicate with the server and send the genre requested to the server
+     * 
+     * @param out     The print writer that is connected to the server
+     * @param message The message that is being sent to the server
+     * @throws IOException
+     */
+    public static void sendMessage(PrintWriter out, String message) throws IOException {
+        out.println(message);
+    }
+
+    /**
+     * Will read in the quotes from the server until the number of quotes has been
+     * reached or till null has been hit
+     * 
+     * @param in        The buffered reader that is connected to the server
+     * @param numQuotes The number of quotes to print from the server
+     * @throws IOException
+     */
+    public static void printQuotes(BufferedReader in, int numQuotes) throws IOException {
+        /* read the date from the socket */
+        String line;
+        int i = 0;
+        while ((line = in.readLine()) != null && i < numQuotes) {
+            System.out.println(line);
+            i++;
+        }
+    }
+
+}
---
+++ QuoteServer.java
@@ -0,0 +1,114 @@
+import java.net.*;
+import java.util.ArrayList;
+import java.util.Random;
+import java.io.*;
+
+/**
+ * Quote Server
+ * Interacts with the QuoteClient to determine how many quotes
+ * it needs to retrieve and what genre to pull the quotes from. Uses the quote
+ * files included with submission to retrieve quotes.
+ * 
+ * Instructions:
+ * -   To compile the file run: javac QuoteServer.java
+ *      from a terminal while in the directory containing the file
+ * -   To run the file (after it has been compiled): java QuoteServer &
+ *      The & puts the QuoteServer in the background
+ * 
+ * CS 376
+ * @author Dalton Rothenberger
+ */
+public class QuoteServer {
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket sock = new ServerSocket(6013);
+
+            // Listening for connections
+            while (true) {
+
+                Socket client = sock.accept();
+
+                // Output to client
+                PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+
+                // Input from client
+                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+                // Getting numQuotes and genre from the client
+                int numQuotes = Integer.parseInt(in.readLine());
+                String genre = in.readLine();
+
+                // Checking if quotes exist for inputted genre
+                File temp = new File(genre+".txt");
+                if (temp.exists()) {
+
+                    // Reading in the file and getting quotes
+                    ArrayList<String> lines = readGenreQuotes(genre);
+                    String quotes = getQuotes(lines, numQuotes);
+
+                    // Sending quotes back to the client
+                    out.println(quotes);
+
+                    // Closing the socket and resuming
+                    client.close();
+
+                } else {
+                    out.println("Error in server: Genre type does not exist\n");
+                }
+
+            }
+
+        } catch (Exception ioe) {
+            System.err.println("Error in server: An exception has occurred.");
+        }
+    }
+
+    /**
+     * Reads in the genre file specifed from the client
+     * 
+     * @param genre The genre which is used to determine the file to read in
+     * @return An string arraylist containing all the quotes from the file
+     * @throws Exception
+     */
+    public static ArrayList<String> readGenreQuotes(String genre) throws Exception {
+
+        ArrayList<String> toReturn = new ArrayList<String>();
+
+        String fileName = genre + ".txt";
+        File file = new File(fileName);
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            toReturn.add(line);
+        }
+
+        br.close();
+        return toReturn;
+    }
+
+    /**
+     * Retrieves the number of quotes specified from the client and appends it all
+     * into one string
+     * 
+     * @param lines     The array list containing the lines from the quote file
+     * @param numQuotes The number of quotes to be collected
+     * @return A string of the quotes appended together
+     */
+    public static String getQuotes(ArrayList<String> lines, int numQuotes) {
+
+        String toReturn = "";
+
+        Random random = new Random();
+
+        for (int i = 0; i < numQuotes; i++) {
+            int randomInt = random.nextInt(lines.size());
+            toReturn += lines.get(randomInt) + "\n";
+        }
+
+        return toReturn;
+    }
+
+}
